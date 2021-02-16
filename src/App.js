04326b8bd532f4786ea7999b5373d46e0327fe48
Neil M <n.mckenzie@live.com>
2021-02-16 10:33:26 -0600

    added monster imgs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component.js'
 import './App.css';
+
 
 class App extends Component {
   constructor() {
@@ -19,10 +21,9 @@ class App extends Component {
   render(){
     return (
       <div className='App'>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+      <CardList monsters ={this.state.monsters}/>
       </div>
+      
     )
   }
 }
